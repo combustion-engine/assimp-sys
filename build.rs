203ceebe331dcb71e2e68720486f0ddb1cc3021f
Aaron Trent <novacrazy@gmail.com>
2017-07-30 19:24:28 -0500

    Update deps and build script

--- build.rs
+++ build.rs
@@ -1,26 +1,46 @@
 extern crate cmake;
+
 use cmake::Config;
 
-#[cfg(not(feature = "double_precision"))]
-fn config_precision(config: &mut Config) {
-    config.define("ASSIMP_DOUBLE_PRECISION", "OFF");
-}
-
-#[cfg(feature = "double_precision")]
-fn config_precision(config: &mut Config) {
-    config.define("ASSIMP_DOUBLE_PRECISION", "ON");
+macro_rules! options {
+    ($config:ident, $($opt:tt = $value:tt,)*) => {
+        $( $config.define(stringify!($opt), stringify!($value)); )*
+    }
 }
 
 fn main() {
     let mut config = Config::new(".");
 
-    config.define("BUILD_SHARED_LIBS", "OFF");
-    config.define("ASSIMP_BUILD_TESTS", "OFF");
-    config.define("ASSIMP_BUILD_ASSIMP_TOOLS", "OFF");
+    options!(config,
+        BUILD_SHARED_LIBS           = OFF,
+        ASSIMP_OPT_BUILD_PACKAGES   = OFF,
+        ASSIMP_ANDROID_JNIIOSYSTEM  = OFF,
+        ASSIMP_NO_EXPORT            = OFF,
+        ASSIMP_BUILD_ZLIB           = OFF,
+        ASSIMP_BUILD_ASSIMP_TOOLS   = OFF,
+        ASSIMP_BUILD_ASSIMP_VIEW    = OFF,
+        ASSIMP_BUILD_SAMPLES        = OFF,
+        ASSIMP_BUILD_TESTS          = OFF,
+        ASSIMP_COVERALLS            = OFF,
+        SYSTEM_IRRXML               = OFF,
+        BUILD_DOCS                  = OFF,
+        ASSIMP_DOUBLE_PRECISION     = OFF,
+        ASSIMP_INSTALL_PDB          = OFF,
+    );
 
-    config_precision(&mut config);
+    if cfg!(feature = "double_precision") {
+        options!(config,
+            ASSIMP_DOUBLE_PRECISION = ON,
+        );
+    }
 
-    let dst = config.build();
+    if cfg!(debug_assertions) {
+        options!(config,
+            ASSIMP_INSTALL_PDB      = ON,
+        );
+    }
+
+    let dst = config.build_target("assimp").build();
 
     println!("cargo:rustc-link-search=native={}", dst.join("lib").display());
     println!("cargo:rustc-link-lib=dylib=assimp");
